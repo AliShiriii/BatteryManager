5d1145b92ddb6d4733cc8bdee1b3de43ea9451f9
AliShiri <Your new email>
2022-07-11 04:11:43 -0700

    add BackgroundInfo

--- app/src/main/java/com/example/batterymanager/MainActivity.kt
+++ app/src/main/java/com/example/batterymanager/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +18,17 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    var batteryInfoBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+    private var batteryInfoBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
 
-            intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
-
+            var batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+            var batteryPlugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)
+            var batteryTemp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
+            var batteryVol = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000
+            Log.e("1010", batteryLevel.toString())
+            Log.e("1010", batteryPlugged.toString())
+            Log.e("1010", batteryTemp.toString())
+            Log.e("1010", batteryVol.toString())
         }
 
     }
