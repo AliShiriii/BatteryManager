d92290bd7fcca541479d7b7f7a891d2e2e054051
AliShiri <Your new email>
2022-07-11 02:59:39 -0700

    add broadcastReceiver

--- app/src/main/java/com/example/batterymanager/MainActivity.kt
+++ app/src/main/java/com/example/batterymanager/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.batterymanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        registerReceiver(batteryInfoBroadcastReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
+    }
+
+    var batteryInfoBroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+
+        }
+
     }
 
 }
