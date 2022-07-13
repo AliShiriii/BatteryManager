0df7fc4816cfa40bd76a76d8bf36c9f7a45af4be
AliShiri <Your new email>
2022-07-13 04:39:58 -0700

    checkPermossion

--- app/src/main/java/com/example/batterymanager/BatteryUsage.kt
+++ app/src/main/java/com/example/batterymanager/BatteryUsage.kt
@@ -3,9 +3,22 @@ package com.example.batterymanager
 import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.Intent
+import android.provider.Settings
 import java.util.*
 
-class BatteryUsage {
+class BatteryUsage(context: Context) {
+
+    init {
+
+        //Check if permission enable
+        if (getUsageStateList(context).isEmpty()) {
+
+            val intent = Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS)
+            context.startActivity(intent)
+
+        }
+    }
 
     private fun getUsageStateList(context: Context): List<UsageStats> {
         val usageStateManagement = getUsageStateManager(context)
@@ -13,7 +26,11 @@ class BatteryUsage {
         val endTime = calendar.timeInMillis
         calendar.add(Calendar.YEAR, -1)
         val startTime = calendar.timeInMillis
-        return usageStateManagement.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, startTime, endTime)
+        return usageStateManagement.queryUsageStats(
+            UsageStatsManager.INTERVAL_DAILY,
+            startTime,
+            endTime
+        )
 
     }
 
